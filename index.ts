cb671b113fbabee83894dc4b01ff3d9cf0d27193
zhouyijun <zhou495248579@hotmail.com>
2020-11-08 22:14:00 +0800

    search 功能

--- index.ts
+++ index.ts
@@ -1,18 +1,42 @@
-import express from 'express';
+import express from "express";
 import * as fs from "fs";
 
-const app = express()
-app.get('/', (req,res)=> {
-    res.send('hello express');
-    res.end()
-})
-app.get('/api/cities', (req,res)=> {
-    fs.readFile('./res/cities.json','utf8', (err,data)=> {
-        if(err) {
-            res.send(err);
-            return;
-        }
-        res.json(JSON.parse(data));
-    })
-})
-app.listen(8080)
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello express");
+  res.end();
+});
+app.get("/api/getCities", (req, res) => {
+  fs.readFile("./res/cities.json", "utf8", (err, data) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    res.json({
+      code: 0,
+      data: JSON.parse(data),
+    });
+  });
+});
+app.get("/api/search", (req, res) => {
+  // @ts-ignore
+  const { key } = req.query;
+  return res.json({
+    result: [
+      {
+        key: "芜湖",
+        display: "芜湖",
+      },
+      {
+        key: "井冈山",
+        display: "井冈山",
+      },
+      {
+        key: "铁岭",
+        display: "铁岭",
+      },
+    ],
+    searchKey: key,
+  });
+});
+app.listen(8080);
