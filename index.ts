efdba7393b789ac1383c96c91a99254f37b8af29
zhouyijun <zhou495248579@hotmail.com>
2020-11-08 13:49:02 +0800

    读取城市功能

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
 import express from 'express';
+import * as fs from "fs";
 
 const app = express()
 app.get('/', (req,res)=> {
     res.send('hello express');
     res.end()
 })
-app.get('/user', (req,res)=> {
-    res.json({
-        code:200,
-        data:[{
-            name:'lilei'
-        }]
-    });
+app.get('/api/cities', (req,res)=> {
+    fs.readFile('./res/cities.json','utf8', (err,data)=> {
+        if(err) {
+            res.send(err);
+            return;
+        }
+        res.json(JSON.parse(data));
+    })
 })
 app.listen(8080)
